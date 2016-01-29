614f50d1d6b3f7775c5e254b83c142c578a1740d
Kevin Wu <wqd1993@qq.com>
2016-01-29 23:16:42 +0800

    Still exploring Gson

--- app/src/test/java/fsyt/ytweatherpro/ExampleUnitTest.java
+++ app/src/test/java/fsyt/ytweatherpro/ExampleUnitTest.java
@@ -1,6 +1,16 @@
 package fsyt.ytweatherpro;
 
+import com.google.gson.Gson;
+
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import fsyt.ytweatherpro.gson.WeatherEntity;
 
 import static org.junit.Assert.*;
 
@@ -11,5 +21,39 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    /**
+    *测试gson实体
+    *@author KevinWu
+    *create at 2016/1/29 22:15
+    */
+    @Test
+    public void testEntity(){
+        String jsonStr=importStr();//导入要测试的数据
+        System.out.println(jsonStr);
+        Gson gson=new Gson();
+        WeatherEntity w=gson.fromJson(jsonStr,WeatherEntity.class);
+        System.out.println(w.reasonEntity.reason);
+    }
+
+    private String importStr(){
+        String str="";
+        File myFile=new File("./json_test.txt");
+        try {
+            FileReader myFileReader=new FileReader(myFile);
+            BufferedReader myBufferedReader=new BufferedReader(myFileReader);
+            String temp;
+            while((temp=myBufferedReader.readLine())!=null){
+                str=str+temp;
+            }
+            myBufferedReader.close();
+            myFileReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
     }
 }
